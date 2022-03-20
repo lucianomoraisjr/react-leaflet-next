a4a66c0209e84ed869cf6289b05061132a687107
unknown <lucianomoraisjr@gmail.com>
2022-03-20 17:56:45 -0300

    criado compatibilidade com react icons

--- src/component/Mapa/index.tsx
+++ src/component/Mapa/index.tsx
@@ -2,6 +2,8 @@ import { MapContainer, TileLayer,  Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import icon from 'leaflet/dist/images/marker-icon.png';
 import { Marker,CustomMarker } from "./CustomMarker"
+import {BiAngry} from 'react-icons/bi'
+import ReactDOMServer from 'react-dom/server';
 
 //import icon from '../../dist/logoEscola.png';
 import L from "leaflet";
@@ -11,6 +13,14 @@ const Map = () => {
        // iconSize: [38, 95],
         iconUrl:'./img/marker-icon.png',
     });
+
+    const mapNodeIcon = L.divIcon({
+        html: ReactDOMServer.renderToString(<BiAngry/>),
+         
+        iconAnchor: [10, 10],
+        popupAnchor: [0, -19],
+        iconSize: [55, 55],
+      });
   
   //@react-icons/all-files/fa/FaBeer
     return (
@@ -23,7 +33,7 @@ const Map = () => {
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <CustomMarker icon={iconPerson} draggable position={[40.8054, -74.0241]} 
+      <CustomMarker icon={mapNodeIcon} draggable position={[40.8054, -74.0241]} 
       
       >
         <Popup>Hey ! I live here</Popup>
